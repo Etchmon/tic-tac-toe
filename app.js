f5ac74718e5feb7ad542a10938846139f7db76e3
James Etchells <etch.rush@gmail.com>
2022-02-14 12:27:05 -0800

    Add DisplayController module

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
+// Function that add X or O to innerhtml of grid buttons when clicked
+
+// Function that changes between placing X or O onclick
+
+// Onclick push the X or O into the gameboard array, no more than 9 answers allowed
+
+// After every X or O placement, run a board check function that checks for 3 in a row
+
+// If 3 in a row announce winner
+
+// Computer function that randomly places a guess
+
 const Gameboard = (() => {
 
-    let gameboard = new Array(9);
+    let gameboard = ['X', '0', 'X', '0', 'X', '0', 'X', '0', 'X'];
 
-    console.log(gameboard);
-})
+    const arrayDisplay = () => {
+        for (i = 0; i < gameboard.length; i++) {
+            document.getElementById('array').innerHTML += gameboard[i];
+        }
+    }
+
+    return {
+        arrayDisplay
+    }
+
+})();
+
+const DisplayController = (() => {
+    // Create array of the gameboard buttons
+    const htmlBoard = Array.from(document.querySelectorAll('button.field'));
+
+    console.log(htmlBoard);
+})();
+
